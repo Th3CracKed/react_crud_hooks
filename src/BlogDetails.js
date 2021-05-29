7aca9b4f716feaee946687e697869e02fce0d2ce
DRIOWYA Abdelghafour <abdodriowya@gmail.com>
2021-05-30 01:11:07 +0200

    support delete blog via detail page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,14 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
-
+    const history = useHistory();
+    const deleteBlog = (blogId) => {
+        fetch('http://localhost:8000/blogs/' + blogId, {
+            method: 'DELETE',
+        }).then(() => history.push('/'))
+    }
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
     return (
         <div className="blog-details">
@@ -14,6 +19,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={() => deleteBlog(blog.id)}>Delete</button>
                 </article>
             )}
         </div>
