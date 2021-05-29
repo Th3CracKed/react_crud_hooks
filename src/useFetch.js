ee6f23d6ef2bc7f3d0b0b778df8e5919c995c7c9
DRIOWYA Abdelghafour <abdodriowya@gmail.com>
2021-05-29 16:33:57 +0200

    simulate network fetch and manage state update on unmounted component

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,23 +4,32 @@ const useFetch = (url) => {
     let [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
+
     useEffect(() => {
-        fetch(url)
-            .then(response => {
-                setIsPending(false);
-                if (!response.ok) {
-                    throw Error('could not fetch the data from that resource');
+        const abortController = new AbortController();
+        setTimeout(() => {
+            fetch(url, { signal: abortController.signal })
+                .then(response => {
+                    setIsPending(false);
+                    if (!response.ok) {
+                        throw Error('could not fetch the data from that resource');
+                    }
+                    response.json()
+                        .then(data => {
+                            setData(data);
+                            setError(null);
+                        });
                 }
-                response.json()
-                    .then(data => {
-                        setData(data);
-                        setError(null);
-                    });
-            }
-            ).catch(err => {
-                setError(err.message);
-                setIsPending(false);
-            });
+                ).catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted');
+                    } else {
+                        setError(err.message);
+                        setIsPending(false);
+                    }
+                });
+        }, 1000);
+        return () => abortController.abort();
     }, [url]);
     return { data, isPending, error };
 }
