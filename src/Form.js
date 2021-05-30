17498becd3100f14270d071190d27ed40cda8583
DRIOWYA Abdelghafour <abdodriowya@gmail.com>
2021-05-30 13:20:15 +0200

    extrat form to seperate component

---
+++ src/Form.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+const Form = ({ fetchMethod }) => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+    const [isPending, setIsPending] = useState(false);
+
+    const history = useHistory();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsPending(true);
+        const blog = { title, body, author };
+        fetchMethod(blog)
+            .then(() => {
+                console.log('new blog added');
+                setIsPending(false);
+                history.push('/');
+            });
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Blog title</label>
+            <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+            <label>Blog body</label>
+            <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+            <label>Blog Author</label>
+            <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                <option value="Mario">Mario</option>
+                <option value="Yoshi">Yoshi</option>
+            </select>
+            {!isPending && <button>Add Blog</button>}
+            {isPending && <button disabled>Adding Blog...</button>}
+        </form>
+    );
+}
+
+export default Form;
